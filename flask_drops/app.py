d61a03376cae4b59fe9c1f3a4616f1cfa857a856
Brito <>
2021-04-19 21:14:39 +0000

    converter url with regex

--- flask_drops/app.py
+++ flask_drops/app.py
@@ -1,8 +1,10 @@
 import db
 from flask import Flask, abort, url_for
+from converters import RegexConverter
+
 
 app = Flask(__name__)
-
+app.url_map.converters['regex'] = RegexConverter
 
 @app.route('/')
 def index():
@@ -23,6 +25,36 @@ def profile(username):
     """
 
 
-app.add_url_rule('/profile/<username>/', view_func=profile, endpoint='user')
+app.add_url_rule('/users/<username>/', view_func=profile, endpoint='user')
+
+
+@app.route('/users/<username>/<int:quote_id>/')
+def quote(username, quote_id):
+    user = db.users.get(username)
+    if user and quote_id:
+        quote = user.get('quote').get(quote_id)
+
+        return f"""<h1>{user['name']}</h1>
+        <img src="{user['image']}" /> <br />
+        Telefone: {user['phone']} <br />
+        <p><q>{quote}</q></p>
+        <hr />"""
+    else:
+        return abort(404, "User not found")
+
+
+@app.route('/files/<path:filename>')
+def filepath(filename):
+    return f"filename: {filename}"
+
+
+@app.route('/reg/<regex("a.*"):name>/')
+def reg(name):
+    return F"Argumento iniciado com a letra a: {name}"
+
+
+@app.route('/reg/<regex("e.*"):name>/')
+def reg_2(name):
+    return F"Argumento iniciado com a letra e: {name}"
 
 app.run(use_reloader=True, host='0.0.0.0', port=8080)
--- flask_drops/db.py
+++ flask_drops/db.py
@@ -3,17 +3,20 @@ users = {
     "leanneg": {
         "name": "Leanne Graham",
         "phone": "1-770-736-8031 x56442",
-        "image": f"https://http.cat/{randint(400, 418)}.jpg"
+        "image": f"https://http.cat/{randint(400, 418)}.jpg",
+        "quote": {2: 'Ops', 1: 'Ops2'}
     },
     "ervinh": {
         "name": "Ervin Howell",
         "phone": "1-770-736-8031 x56442",
-        "image": f"https://http.cat/{randint(400, 418)}.jpg"
+        "image": f"https://http.cat/{randint(400, 418)}.jpg",
+        "quote": {1: 'e aí'}
     },
     "nicholasr": {
         "name": "Nicholas Runolfsdottir",
         "phone": "1-770-736-8031 x56442",
-        "image": f"https://http.cat/{randint(400, 418)}.jpg"
+        "image": f"https://http.cat/{randint(400, 418)}.jpg",
+        "quote": {3: 'Eita', 1: 'Eureka', 2: 'vixi'}
     },
 }
 
